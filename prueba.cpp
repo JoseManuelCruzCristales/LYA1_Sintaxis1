1b9498de518e228dfa1507fef5434faa7ca6bf99
JoseManuelCruzCristales <l21160613@queretaro.tecnm.mx>
2023-11-14 22:45:44 -0600

    Proyecto con 2 requerimientos

---
+++ prueba.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdio>
+#include <math>
+
+char  a;
+int   a,b,c,d,radio;
+float x,y,z;
+char  w;
+
+void main()
+{
+    printf("\nHola mundo");
+    printf("\n============");
+    printf("\nRadio = ",a
+    );
+    scanf("hola",&radio);
+
+    radio = (3+5)*8-(10-4)/2;
+
+    z = (a+b)*c;
+
+    if ((3+5)*8 > z)
+    {
+        printf("\nEs mayor",z);
+    } 
+
+    if (radio % 2 == 0)
+    {
+        printf("\nRadio es par");
+    }
+}
